5e4a6b50a2c059efaee6f866ea0dd1d3efd3d8b9
MrKingofspades <ericvining_70@yahoo.com>
2023-07-10 22:51:56 -0700

    Update BattleSystem.java
    Edited spacing

--- Monster_Battler_Dating_Sim/src/Stats/BattleSystem.java
+++ Monster_Battler_Dating_Sim/src/Stats/BattleSystem.java
@@ -12,37 +12,36 @@ public class BattleSystem {
 			this.lvl = 1;
 		}
 		this.exp = (int) Math.pow(lvl * 2, 1.7);
-;
 	}
 
-		// What to call when the enemy appears, initiates the battle
-		public void enemyAppears() {
-			System.out.println("A wild " + monster + " appears before you!\n");
-		}
+	// What to call when the enemy appears, initiates the battle
+	public void enemyAppears() {
+		System.out.println("A wild " + monster + " appears before you!\n");
+	}
+	
+	// Check winner, if player health is less than or equal to zero, player loses
+	// if enemy's health is less than or equal to zero, player wins
+	public void checkWinner() {
 		
-		// Check winner, if player health is less than or equal to zero, player loses
-		// if enemy's health is less than or equal to zero, player wins
-		public void checkWinner() {
-			
-		}
-		
-		public int getExp() {
-			return exp;
-		}
-		
-		public int getLvl() {
-			return lvl;
-		}
-		
-		public String getMonster() {
-			return monster;
-		}
-		
-		public void setMonster(String monster) {
-			this.monster = monster;
-		}
-		
-		public void setLvl(int lvl) {
-			this.lvl = lvl;
-		}
+	}
+	
+	public int getExp() {
+		return exp;
+	}
+	
+	public int getLvl() {
+		return lvl;
+	}
+	
+	public String getMonster() {
+		return monster;
+	}
+	
+	public void setMonster(String monster) {
+		this.monster = monster;
+	}
+	
+	public void setLvl(int lvl) {
+		this.lvl = lvl;
+	}
 }
